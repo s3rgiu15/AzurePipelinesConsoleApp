ef53fd5ece89001f203f67b009ee672c7ef710c4
Bogdan Petea <Peteab@kynetix.com>
2019-06-13 10:20:26 +0100

    Add initial AzirePipelinesConsole app

---
+++ AzurePipelinesConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzurePipelinesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Azure Pipelines, Kynteix!");
+            Console.ReadLine();
+        }
+    }
+}
