98a17f174b773fbc894205f2489161a7aa96b24d
s3rgiu15 <mitrasca.sergiu@gmail.com>
2019-06-14 11:44:27 +0300

    Change main class to print AppSettings variable

--- AzurePipelinesConsole/Program.cs
+++ AzurePipelinesConsole/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ namespace AzurePipelinesConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Azure Pipelines, Kynteix!");
+
+            string releaseVariable = ConfigurationSettings.AppSettings["releaseVariable"];
+            Console.WriteLine("Release var: " + releaseVariable);
+
             Console.ReadLine();
         }
     }
